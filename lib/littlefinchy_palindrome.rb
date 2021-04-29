2f8d7d5edf93108c0a8b38e975301749f290508d
LittleFinchy <smfinch99@gmail.com>
2021-04-28 22:23:02 -0400

    init

---
+++ lib/littlefinchy_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "littlefinchy_palindrome/version"
+
+module LittlefinchyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
